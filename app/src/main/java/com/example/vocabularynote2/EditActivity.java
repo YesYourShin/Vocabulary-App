44d7db8cdda443f046b3aad19f277c992733b4da
Shin <ehrl827@naver.com>
2021-05-18 20:29:23 +0900

    upload

---
+++ app/src/main/java/com/example/vocabularynote2/EditActivity.java
@@ -0,0 +1,83 @@
+package com.example.vocabularynote2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EditActivity extends AppCompatActivity {
+
+    private ListView editList;
+    private ArrayList<ListData> arrayList;
+    private ListAdapter listAdapter;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private int count = 0;
+
+    private TextView edit_kanji;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+//        editList = (ListView)findViewById(R.id.editList);
+//
+//        List<String> data = new ArrayList<>();
+//
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,data);
+//        editList.setAdapter(adapter);
+//
+//        data.add("日本の大学に留学しています。");
+//        data.add("みんな教室では日本語で話します。");
+//        data.add("学食はとても便利です。");
+//        adapter.notifyDataSetChanged();
+
+
+        edit_kanji = findViewById(R.id.edit_kanji);
+
+        Intent intent = getIntent();
+        String kanji = intent.getStringExtra("kanji");
+
+
+
+
+        recyclerView = (RecyclerView)findViewById(R.id.rv);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        arrayList = new ArrayList<>();
+
+        listAdapter = new ListAdapter(arrayList);
+        recyclerView.setAdapter(listAdapter);
+
+        Button btn_add = (Button)findViewById(R.id.btn_add);
+        btn_add.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EditActivity.this, EditItem.class);
+                startActivity(intent);
+                ListData listData = new ListData(Integer.toString(arrayList.size()+1),kanji,"hatsuon","imi");
+                arrayList.add(listData);
+                listAdapter.notifyDataSetChanged();
+
+
+            }
+        });
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/vocabularynote2/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.vocabularynote2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+    private ListView list;
+    private FloatingActionButton addList;
+
+    private ArrayList<VNoteData> arrayList;
+    private VNoteAdapter vNoteAdapter;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        list = (ListView)findViewById(R.id.list);
+//
+//        List<String> data = new ArrayList<>();
+//
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,data);
+//        list.setAdapter(adapter);
+//
+//        data.add("사과");
+//        data.add("바나나");
+//        data.add("포도");
+//        adapter.notifyDataSetChanged();
+
+
+        recyclerView = (RecyclerView)findViewById(R.id.mrv);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager((linearLayoutManager));
+
+        arrayList = new ArrayList<>();
+
+        vNoteAdapter = new VNoteAdapter(arrayList);
+        recyclerView.setAdapter(vNoteAdapter);
+
+
+
+        addList = findViewById(R.id.addList);
+        addList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, EditActivity.class);
+                startActivity(intent);
+
+                VNoteData vNoteData = new VNoteData(Integer.toString(arrayList.size()+1));
+                arrayList.add(vNoteData);
+                vNoteAdapter.notifyDataSetChanged();
+
+            }
+        });
+
+
+
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawerView = (View)findViewById(R.id.drawer);
+
+        drawerLayout.setDrawerListener(listener);
+        drawerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+
+    }
+
+    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+
+        }
+    };
+}
